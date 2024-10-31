32f52905b4393fd0bbd28ce0aa1c8398376ce60c
Rishat <virisound@gmail.com>
2024-10-31 06:27:21 +0500

    Fixed API - request to VPS: 'http://212.22.70.27/api/' in axios.api.ts

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -5,7 +5,8 @@ import { getTokenFromLocalStorage } from "../helpers/localStorage.helper";
 
 //http://localhost:3000/api/
 export const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    // baseURL: 'http://localhost:3000/api/', // для локальной машины
+    baseURL: 'http://212.22.70.27/api/', // для VPS сервера с его публичным IP (если в конфиге Nginx прописан прокси порт, то 3000 не нужно дописывать в пути)
     // timeout: 1000,
     headers: {
         Authorization: `Bearer ` + getTokenFromLocalStorage() || '' // при любом (кроме регистрации) обращении к server достаем из отправляем токен (так требует @UseGuards(JwtAuthGuard) в server\src\auth\auth.controller.ts)
